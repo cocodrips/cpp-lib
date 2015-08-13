22efdab192d569cc7be9899779f78bcd54fd7521
ku-mu <coco@itolab.is.ocha.ac.jp>
2015-08-13 17:03:13 +0900

    Create StringSplit.cpp

---
+++ StringSplit.cpp
@@ -0,0 +1,6 @@
+std::vector<std::string> split(const std::string &str, char sep)
+{
+    std::vector<std::string> v; std::stringstream ss(str); std::string buffer;
+    while(std::getline(ss, buffer, sep) ) v.push_back(buffer);
+    return v;
+}
